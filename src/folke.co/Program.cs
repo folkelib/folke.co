06554c7d2bf64de9a2dc4a74981faf2f2c4edbb6
Arnaud Castaner <arnaud.castaner@outlook.com>
2016-08-11 14:19:52 +0200

    First commit

---
+++ src/folke.co/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Folke;
+using Microsoft.Extensions.Configuration;
+
+namespace folke.co
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // We load the configuration here so that the app will listen on a port configurable from the appsettings.json file ("server.urls" section)
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddJsonFile("appsettings.Local.json", optional: true)
+                    .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/folke.co/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Folke.Core;
+using Folke.Elm.Mysql;
+using Microsoft.Extensions.Configuration;
+using Folke.Elm;
+using Microsoft.AspNetCore.Identity;
+using Folke.Core.Entities;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace Folke
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.Local.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddFolkeCore<MySqlDriver>(options =>
+            {
+                options.ConnectionString = Configuration["Data:ConnectionString"];
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IFolkeConnection connection,
+            RoleManager<Role> roleManager, UserManager<User> userManager, ApplicationPartManager applicationPartManager)
+        {
+            loggerFactory.AddConsole();
+            app.UseMvc();
+
+            app.UseFolkeCore(connection, env, roleManager, userManager, applicationPartManager, options =>
+            {
+                options.AdministratorEmail = Configuration["Data:DefaultAdministratorUserName"];
+                options.AdministratorPassword = Configuration["Data:DefaultAdministratorPassword"];
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
